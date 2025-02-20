ae8cbe471313bd6a2c733452bbe6cadd67dd720e
Rushi <rushin.mailme@gmail.com>
2025-02-21 01:08:13 +0530

    Login UI Done

--- TaskFlow/Controllers/LoginController.cs
+++ TaskFlow/Controllers/LoginController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TaskFlow.Models;
 
 namespace TaskFlow.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // To Be Implemented - Login Functionality
+            }
+            return View("Index");
         }
     }
 }
